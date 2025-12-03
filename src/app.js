acd5205eec14de6a3c393fe74d0f1e9fa1a893a7
Nirav Patel <127055509+nrpatel370@users.noreply.github.com>
2025-12-02 21:49:54 -0500

    added trade analysis service

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const rosterRoutes = require('./routes/rosterRoutes');
 const playerRoutes = require('./routes/playerRoutes');
 const rankingsRoutes = require('./routes/rankingsRoutes');
+const tradeRoutes = require('./routes/tradeRoutes');
 const app = express();
 
 // Middleware
@@ -22,6 +23,8 @@ app.use('/api/users', userRoutes);
 app.use('/api/rosters', rosterRoutes);
 app.use('/api/players', playerRoutes);
 app.use('/api/rankings', rankingsRoutes);
+app.use('/api/trade', tradeRoutes);
+
 
 // Health check
 app.get('/api/health', (req, res) => {
